dda93e88426fc13ea63f0709c776dc5f780fb6c7
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2022-07-17 04:06:00 +0000

    add empty rightmost column with window.innerWidth

--- out/csv.js
+++ out/csv.js
@@ -374,6 +374,9 @@ function handleEvents() {
             content.bindings.forEach((b) => {
                 flex.columns.push(new wijmo.grid.Column(b));
             });
+            flex.columns.push(new wijmo.grid.Column({
+                width: window.innerWidth
+            }));
             flex.itemsSource = content.data;
             flex.endUpdate();
         }
